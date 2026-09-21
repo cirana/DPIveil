85c4b6212ba256d3dbae2e515ad02a8b24dffd91
yurtina <105820307+yurtina@users.noreply.github.com>
2026-09-21 23:21:55 +0300

    refactor: centralize Discord domain constants

---
+++ dpiveil/constants.py
@@ -0,0 +1,17 @@
+"""Shared project constants."""
+
+DISCORD_DOMAINS = (
+    "discord.com",
+    "discord.gg",
+    "discordapp.com",
+    "discordapp.net",
+    "discord.media",
+    "discordcdn.com",
+    "discord.dev",
+    "discord.new",
+    "discord.gift",
+    "discordstatus.com",
+    "dis.gd",
+    "discord.co",
+    "discord-attachments-uploads-prd.storage.googleapis.com",
+)
